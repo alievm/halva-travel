1138a4ac19584d7857f3f1492e4268f1a54b63a1
alievm <alievmmm111@gmail.com>
2025-04-30 23:22:23 +0500

    fixed

--- halva-travel/src/pages/news/news-detail/index.jsx
+++ halva-travel/src/pages/news/news-detail/index.jsx
@@ -6,6 +6,7 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import getAppLang from "../../../utils/getAppLang";
 
 
 
